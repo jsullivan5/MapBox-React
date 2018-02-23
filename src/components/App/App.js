7b4a7f1ca98fd6d8a808f585a24a0ccdebe054d8
James Sullivan <jamesfsullivan5@gmail.com>
2018-02-23 13:29:04 -0700

    Add getChildFunc to App and add clickEvent to anchor tag in location node

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,25 +5,30 @@ import './App.css';
 
 class App extends Component {
   constructor() {
+    super();
     this.state = {
-      locationNodes: [];
+      locationNodes: [],
     }
     this.buildLocationList = this.buildLocationList.bind(this);
+    this.getChildFunc = this.getChildFunc.bind(this);
   }
 
   buildLocationList(data) {
+    const { flyToStore, createPopUp } = this.state;
+
     const locationNodes = data.features.map((feature, i) => {
       const prop = feature.properties
       const { phone } = prop;
 
       return (
         <div key={'listing-' + i} className="item">
-          <a href="#" className="title" dataPosition={i}>
+          <a className="title" data-position={i}
+            onClick={() => flyToStore(feature)}
+          >
             {prop.address}
           </a>
           <div>
-            <p>{prop.city}</p>
-            <p>{phone && ' &middot; ' + prop.phoneFormatted}</p>
+            {prop.city} {phone && ' : ' + prop.phoneFormatted}
           </div>
         </div>
       );
@@ -32,13 +37,21 @@ class App extends Component {
     this.setState({ locationNodes });
   }
 
+  getChildFunc(func) {
+    const formattedName = func.name.replace('bound', '').trim();
+    this.setState({ [formattedName]: func });
+  }
+
   render() {
     const { locationNodes } = this.state;
 
     return (
       <div className="App">
         <SideBar locationNodes={locationNodes} />
-        <MapBox buildLocationList={this.buildLocationList} />
+        <MapBox
+          buildLocationList={this.buildLocationList}
+          getChildFunc={this.getChildFunc}
+        />
       </div>
     );
   }
