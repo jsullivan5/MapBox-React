2faf22ce6262bff9796538a8dc50fd51b9979472
James Sullivan <jamesfsullivan5@gmail.com>
2018-02-23 11:49:31 -0700

    Add buildLocationList method to App and pass down props to children

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,44 @@
 import React, { Component } from 'react';
 import MapBox from '../MapBox/MapBox';
+import SideBar from '../SideBar/SideBar';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    this.state = {
+      locationNodes: [];
+    }
+    this.buildLocationList = this.buildLocationList.bind(this);
+  }
+
+  buildLocationList(data) {
+    const locationNodes = data.features.map((feature, i) => {
+      const prop = feature.properties
+      const { phone } = prop;
+
+      return (
+        <div key={'listing-' + i} className="item">
+          <a href="#" className="title" dataPosition={i}>
+            {prop.address}
+          </a>
+          <div>
+            <p>{prop.city}</p>
+            <p>{phone && ' &middot; ' + prop.phoneFormatted}</p>
+          </div>
+        </div>
+      );
+    });
+
+    this.setState({ locationNodes });
+  }
+
   render() {
+    const { locationNodes } = this.state;
+
     return (
       <div className="App">
-        <MapBox />
+        <SideBar locationNodes={locationNodes} />
+        <MapBox buildLocationList={this.buildLocationList} />
       </div>
     );
   }
