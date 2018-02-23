35f616c51a67da8a6635ac421defddff85176b6e
James Sullivan <jamesfsullivan5@gmail.com>
2018-02-23 13:26:47 -0700

    Add flyToStore and createPopup functions to MapBox and pass up to App

--- src/components/MapBox/MapBox.js
+++ src/components/MapBox/MapBox.js
@@ -5,7 +5,16 @@ import { stores } from '../../sweetgreen';
 mapBoxGl.accessToken = 'pk.eyJ1IjoianN1bGxpdmFuNSIsImEiOiJjamR6MWc2dmowZDFsMzNtb3RtdTJ3bWR6In0.dNpIQ0o88Vz-eEu2pITqdA';
 
 class MapBox extends Component {
+  constructor(props) {
+    super(props);
+
+    this.flyToStore = this.flyToStore.bind(this);
+    this.createPopUp = this.createPopUp.bind(this);
+  }
+
   componentDidMount() {
+    const { buildLocationList, getChildFunc } = this.props;
+
     this.map = new mapBoxGl.Map({
       container: this.mapContainer,
       style: 'mapbox://styles/mapbox/light-v9',
@@ -29,11 +38,35 @@ class MapBox extends Component {
           'icon-allow-overlap': true,
         }
       });
+
+      buildLocationList(stores);
     });
+
+    getChildFunc(this.flyToStore);
+    getChildFunc(this.createPopUp);
   }
 
   componentWillUnmount() {
     this.map.remove();
+  }
+
+  flyToStore(currentFeature) {
+    this.map.flyTo({
+      center: currentFeature.geometry.coordinates,
+      zoom: 15
+    });
+  }
+
+  createPopUp(currentFeature) {
+    const popUps = document.getElementsByClassName('mapboxgl-popup');
+    // Check if there is already a popup on the map and if so, remove it
+    if (popUps[0]) { popUps[0].remove(); }
+
+    const popup = new mapBoxGl.Popup({ closeOnClick: false })
+      .setLngLat(currentFeature.geometry.coordinates)
+      .setHTML('<h3>Sweetgreen</h3>' +
+        '<h4>' + currentFeature.properties.address + '</h4>')
+      .addTo(this.map);
   }
 
   render() {
