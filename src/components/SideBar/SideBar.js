97cfa5ee64b3fce15f4bf18bb2ab8232c2ad54b6
James Sullivan <jamesfsullivan5@gmail.com>
2018-02-23 13:27:31 -0700

    Add listings className to locationNode container and modify render condition

--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -3,10 +3,15 @@ import React, { Component } from 'react';
 class SideBar extends Component {
 
   render() {
+    const { locationNodes } = this.props;
+
     return (
       <div className='sidebar pad2'>
         <div className='heading'>
           <h1>Our locations</h1>
+          <div className="listings">
+            {locationNodes.length > 0 && locationNodes}
+          </div>
         </div>
         <div id='listings' className='listings'></div>
       </div>
