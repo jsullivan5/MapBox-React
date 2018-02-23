07b3a47bb276d5b7a9f7e6f3be1b93be7863dd3b
James Sullivan <jamesfsullivan5@gmail.com>
2018-02-23 15:28:45 -0700

    Add buildLocationList to SideBar

--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -1,9 +1,45 @@
 import React, { Component } from 'react';
 
 class SideBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      stores: this.props.stores.features,
+      activeStore: ''
+    }
+  }
+  buildLocationList() {
+    const { stores, activeStore } = this.state;
+    const { handleListingClick } = this.props;
+
+    return stores.map((store, i) => {
+      const prop = store.properties
+      const { phone } = prop;
+      const listingId = 'listing-' + i;
+
+      return (
+        <div
+          key={listingId}
+          className={`item ${prop.address === activeStore ? 'active': ''}`}
+          onClick={() => {
+            handleListingClick(store);
+            this.setState({ activeStore: prop.address });
+          }}
+        >
+          <a className="title" data-position={i}>
+            {prop.address}
+          </a>
+          <div>
+            {prop.city} {phone && ' : ' + prop.phoneFormatted}
+          </div>
+        </div>
+      );
+    });
+  }
 
   render() {
-    const { locationNodes } = this.props;
+    const { stores } = this.state;
+    const locationNodes = this.buildLocationList();
 
     return (
       <div className='sidebar pad2'>
