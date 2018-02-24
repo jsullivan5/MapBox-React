2098bfee4e3ba4fb9f172828855a08ae540ca2bc
James Sullivan <jamesfsullivan5@gmail.com>
2018-02-23 18:46:48 -0700

    Add click event to map in MapBox component

--- src/components/MapBox/MapBox.js
+++ src/components/MapBox/MapBox.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import mapBoxGl from 'mapbox-gl/dist/mapbox-gl.js';
-import { stores } from '../../sweetgreen';
 
 mapBoxGl.accessToken = 'pk.eyJ1IjoianN1bGxpdmFuNSIsImEiOiJjamR6MWc2dmowZDFsMzNtb3RtdTJ3bWR6In0.dNpIQ0o88Vz-eEu2pITqdA';
 
@@ -10,10 +9,11 @@ class MapBox extends Component {
 
     this.flyToStore = this.flyToStore.bind(this);
     this.createPopUp = this.createPopUp.bind(this);
+    this.handleListingClick = this.handleListingClick.bind(this);
   }
 
   componentDidMount() {
-    const { buildLocationList, getChildFunc } = this.props;
+    const { getActiveStore, getChildFunc, stores } = this.props;
 
     this.map = new mapBoxGl.Map({
       container: this.mapContainer,
@@ -38,12 +38,33 @@ class MapBox extends Component {
           'icon-allow-overlap': true,
         }
       });
-
-      buildLocationList(stores);
     });
 
-    getChildFunc(this.flyToStore);
-    getChildFunc(this.createPopUp);
+    const map = this.map; // weird this binding in click event
+
+    this.map.on('click', (e) => {
+      // Query all the rendered points in the view
+      const features = map.queryRenderedFeatures(e.point, { layers: ['locations'] });
+
+      if (features.length) {
+        var clickedPoint = features[0];
+        // 1. Fly to the point
+        this.flyToStore(clickedPoint);
+        // 2. Close all other popups and display popup for clicked store
+        this.createPopUp(clickedPoint);
+        // 3. Highlight listing in sidebar (and remove highlight for all other listings)
+        // var activeItem = document.getElementsByClassName('active');
+        // if (activeItem[0]) {
+        //   activeItem[0].classList.remove('active');
+        // }
+        // Find the index of the store.features that corresponds to the clickedPoint that fired the event listener
+        const selectedFeature = clickedPoint.properties.address;
+
+        getActiveStore(selectedFeature);
+      }
+    });
+
+    getChildFunc(this.handleListingClick);
   }
 
   componentWillUnmount() {
@@ -62,22 +83,26 @@ class MapBox extends Component {
     // Check if there is already a popup on the map and if so, remove it
     if (popUps[0]) { popUps[0].remove(); }
 
-    const popup = new mapBoxGl.Popup({ closeOnClick: false })
+    return new mapBoxGl.Popup({ closeOnClick: false })
       .setLngLat(currentFeature.geometry.coordinates)
       .setHTML('<h3>Sweetgreen</h3>' +
         '<h4>' + currentFeature.properties.address + '</h4>')
       .addTo(this.map);
   }
 
-  render() {
-    const style = {
-      position: 'absolute',
-      top: 0,
-      bottom: 0,
-      width: '100%'
-    };
+  handleListingClick(listing) {
+    this.flyToStore(listing);
+    this.createPopUp(listing);
+  }
 
-    return <div className="map" ref={el => this.mapContainer = el} />;
+  render() {
+    return (
+      <div
+        className="map"
+        ref={el => this.mapContainer = el}
+        onClick={this.handleMapClick}
+      />
+    );
   }
 }
 
