3f423a5cd6e8e0105b59d62cb2d1f14e6a501e0f
James Sullivan <jamesfsullivan5@gmail.com>
2018-02-25 13:42:07 -0700

    Add store.js for redux

---
+++ src/store.js
@@ -0,0 +1,26 @@
+/* eslint-disable no-underscore-dangle */
+
+import { createStore, applyMiddleware, compose } from 'redux';
+import rootReducer from './reducers';
+import storeGeoJson from './sweetgreenGeoJson';
+
+const initialState = { stores: storeGeoJson };
+const enhancers = [];
+const middleWare = [];
+
+if (window.__REDUX_DEVTOOLS_EXTENSION__) {
+  enhancers.push(window.__REDUX_DEVTOOLS_EXTENSION__());
+}
+
+const composeEnhancers = compose(
+  applyMiddleware(...middleWare),
+  ...enhancers,
+);
+
+const store = createStore(
+  rootReducer,
+  initialState,
+  composeEnhancers,
+);
+
+export default store;
