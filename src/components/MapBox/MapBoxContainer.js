4b1d7db8ce1946af0b95175ad4cb58212ff569d4
James Sullivan <jamesfsullivan5@gmail.com>
2018-02-25 14:32:03 -0700

    Add setActiveStore in mapDispatchToProps to MapBoxContainer

--- src/components/MapBox/MapBoxContainer.js
+++ src/components/MapBox/MapBoxContainer.js
@@ -1,8 +1,13 @@
 import { connect } from 'react-redux';
 import MapBox from './MapBox';
+import { setActiveStore } from '../../actions';
 
 const mapStatToProps = ({ stores }) => ({
   stores,
 });
 
-export default connect(mapStatToProps, null)(MapBox);
+const mapDispatchToProps = dispatch => ({
+  setActiveStore: store => dispatch(setActiveStore(store)),
+});
+
+export default connect(mapStatToProps, mapDispatchToProps)(MapBox);
