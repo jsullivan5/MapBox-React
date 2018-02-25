54c39046d613a79e93ab51c8bd30c15418977fb4
James Sullivan <jamesfsullivan5@gmail.com>
2018-02-25 16:07:51 -0700

    Move Map initialization to actions and init via props

--- src/components/MapBox/MapBox.js
+++ src/components/MapBox/MapBox.js
@@ -8,78 +8,17 @@ class MapBox extends Component {
   constructor(props) {
     super(props);
 
-    this.createPopUp = this.createPopUp.bind(this);
     this.handleListingClick = this.handleListingClick.bind(this);
   }
 
   componentDidMount() {
-    const { stores, setActiveStore, flyToStore } = this.props;
+    const { initializeMap } = this.props;
 
-    this.mapBox = new mapBoxGl.Map({
-      container: this.mapContainer,
-      style: 'mapbox://styles/mapbox/light-v9',
-      center: [-77.034084, 38.909671],
-      zoom: 14,
-    });
-
-    // Add the data to your map as a layer
-    this.mapBox.on('load', () => {
-      // Add the data to your map as a layer
-      this.mapBox.addLayer({
-        id: 'locations',
-        type: 'symbol',
-        // Add a GeoJSON source containing place coordinates and information.
-        source: {
-          type: 'geojson',
-          data: stores,
-        },
-        layout: {
-          'icon-image': 'restaurant-15',
-          'icon-allow-overlap': true,
-        },
-      });
-    });
-
-    this.mapBox.on('click', (e) => {
-      // Query all the rendered points in the view
-      const features = this.mapBox.queryRenderedFeatures(e.point, { layers: ['locations'] });
-
-      if (features.length) {
-        const clickedPoint = features[0];
-        // 1. Fly to the point
-        flyToStore(this.mapBox, clickedPoint);
-        // 2. Close all other popups and display popup for clicked store
-        this.createPopUp(clickedPoint);
-        // 3. Highlight listing in sidebar (and remove highlight for all other listings)
-        // var activeItem = document.getElementsByClassName('active');
-        // if (activeItem[0]) {
-        //   activeItem[0].classList.remove('active');
-        // }
-        // Find the index of the store.features that corresponds to
-        // the clickedPoint that fired the event listener
-        const selectedFeature = clickedPoint.properties.address;
-
-        setActiveStore(selectedFeature);
-      }
-    });
+    this.mapBox = initializeMap(this.mapContainer);
   }
 
   componentWillUnmount() {
     this.mapBox.remove();
-  }
-
-  createPopUp(currentFeature) {
-    const popUps = document.getElementsByClassName('mapboxgl-popup');
-    // Check if there is already a popup on the map and if so, remove it
-    if (popUps[0]) { popUps[0].remove(); }
-
-    return new mapBoxGl.Popup({ closeOnClick: false })
-      .setLngLat(currentFeature.geometry.coordinates)
-      .setHTML(`
-        <h3>Sweetgreen</h3>
-        <h4>${currentFeature.properties.address}</h4>
-      `)
-      .addTo(this.mapBox);
   }
 
   handleListingClick(listing) {
@@ -100,9 +39,8 @@ class MapBox extends Component {
 }
 
 MapBox.propTypes = {
-  setActiveStore: propTypes.func.isRequired,
   flyToStore: propTypes.func.isRequired,
-  stores: propTypes.object.isRequired,
+  initializeMap: propTypes.func.isRequired,
 };
 
 export default MapBox;
