f8e658697a3ea71881f86f8ae8ade6c2c381b499
James Sullivan <jamesfsullivan5@gmail.com>
2018-02-25 17:21:16 -0700

    Refactor App to stateless component and add main wrapper for semantics

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,44 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 import MapBoxContainer from '../MapBox/MapBoxContainer';
-import SideBar from '../SideBar/SideBar';
-import storesGeoJson from '../../sweetgreenGeoJson';
+import SideBarContainer from '../SideBar/SideBarContainer';
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      stores: storesGeoJson,
-      activeStore: '',
-    };
-
-    this.getChildFunc = this.getChildFunc.bind(this);
-    this.getActiveStore = this.getActiveStore.bind(this);
-  }
-
-  getChildFunc(func) {
-    const formattedName = func.name.replace('bound', '').trim();
-    this.setState({ [formattedName]: func });
-  }
-
-  getActiveStore(storeAddress) {
-    this.setState({ activeStore: storeAddress });
-  }
-
-  render() {
-    const { stores, handleListingClick, activeStore } = this.state;
-
-    return (
-      <div className="App">
-        <SideBar
+const App = () => (
+  <main className="App">
+    {/* <SideBar
           stores={stores}
           activeStore={activeStore}
           getActiveStore={this.getActiveStore}
           handleListingClick={handleListingClick}
-        />
-        <MapBoxContainer />
-      </div>
-    );
-  }
-}
+        /> */}
+    <SideBarContainer />
+    <MapBoxContainer />
+  </main>
+);
+
 
 export default App;
