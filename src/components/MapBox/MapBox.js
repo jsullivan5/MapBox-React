429e62ea44b3bc9c381acf6108538a51855c8f8a
James Sullivan <jamesfsullivan5@gmail.com>
2018-02-25 17:47:37 -0700

    Remove unused methods and mapbox imports from MapBox component

--- src/components/MapBox/MapBox.js
+++ src/components/MapBox/MapBox.js
@@ -1,31 +1,14 @@
 import React, { Component } from 'react';
 import propTypes from 'prop-types';
-import mapBoxGl from 'mapbox-gl/dist/mapbox-gl';
-
-mapBoxGl.accessToken = 'pk.eyJ1IjoianN1bGxpdmFuNSIsImEiOiJjamR6MWc2dmowZDFsMzNtb3RtdTJ3bWR6In0.dNpIQ0o88Vz-eEu2pITqdA';
 
 class MapBox extends Component {
-  constructor(props) {
-    super(props);
-
-    this.handleListingClick = this.handleListingClick.bind(this);
-  }
-
   componentDidMount() {
     const { initializeMap } = this.props;
-
     this.mapBox = initializeMap(this.mapContainer);
   }
 
   componentWillUnmount() {
     this.mapBox.remove();
-  }
-
-  handleListingClick(listing) {
-    const { flyToStore } = this.props;
-
-    flyToStore(this.mapBox, listing);
-    this.createPopUp(listing);
   }
 
   render() {
@@ -39,7 +22,6 @@ class MapBox extends Component {
 }
 
 MapBox.propTypes = {
-  flyToStore: propTypes.func.isRequired,
   initializeMap: propTypes.func.isRequired,
 };
 
